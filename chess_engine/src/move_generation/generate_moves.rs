f4b634f18c7099336eb4b1ca13990da6ee76fa43
Renzo <renzo.boenink@gmail.com>
2026-02-22 21:14:16 +0100

    Implement sliding moves

--- chess_engine/src/move_generation/generate_moves.rs
+++ chess_engine/src/move_generation/generate_moves.rs
@@ -126,19 +126,68 @@ fn generate_knight_moves(board: &Board, moves: &mut [Move], curr_move_index: &mu
     }
 }
 
-fn generate_bishop_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
-
-
+pub fn generate_bishop_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
+    let us = if board.white_to_move { Piece::WHITE } else { Piece::BLACK };
+    let bishops = board.pieces[(us | Piece::BISHOP) as usize];
+    generate_sliding_moves(board, moves, curr_move_index, bishops, &BISHOP_SHIFTS);
 }
 
-fn generate_rook_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
-
-
+pub fn generate_rook_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
+    let us = if board.white_to_move { Piece::WHITE } else { Piece::BLACK };
+    let rooks = board.pieces[(us | Piece::ROOK) as usize];
+    generate_sliding_moves(board, moves, curr_move_index, rooks, &ROOK_SHIFTS);
 }
 
-fn generate_queen_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
+pub fn generate_queen_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
+    let us = if board.white_to_move { Piece::WHITE } else { Piece::BLACK };
+    let queens = board.pieces[(us | Piece::QUEEN) as usize];
 
+    // A Queen moves like a Bishop AND a Rook
+    generate_sliding_moves(board, moves, curr_move_index, queens, &BISHOP_SHIFTS);
+    generate_sliding_moves(board, moves, curr_move_index, queens, &ROOK_SHIFTS);
+}
 
+fn generate_sliding_moves(
+    board: &Board,
+    moves: &mut [Move],
+    curr_move_index: &mut usize,
+    mut piece_bb: u64, // The bitboard of pieces to generate moves for
+    shifts: &[(i8, u64)], // The directions [(offset, mask)]
+) {
+    let us = if board.white_to_move { Piece::WHITE } else { Piece::BLACK };
+    let enemy_occ = board.pieces[((us ^ 8) | Piece::ALL) as usize];
+    let friendly_occ = board.pieces[(us | Piece::ALL) as usize];
+
+    while piece_bb != 0 {
+        let from = piece_bb.trailing_zeros() as u8;
+        let from_bit = 1u64 << from;
+
+        for &(shift, mask) in shifts {
+            let mut current_sq_mask = from_bit;
+            loop {
+                if shift > 0 {
+                    current_sq_mask = (current_sq_mask << shift) & mask;
+                } else {
+                    current_sq_mask = (current_sq_mask >> shift.abs()) & mask;
+                }
+
+                if current_sq_mask == 0 { break; }
+
+                let to = current_sq_mask.trailing_zeros() as u8;
+                if (current_sq_mask & friendly_occ) != 0 { break; }
+
+                if (current_sq_mask & enemy_occ) != 0 {
+                    moves[*curr_move_index] = Move::new_with_flags(from, to, Move::CAPTURE);
+                    *curr_move_index += 1;
+                    break;
+                }
+
+                moves[*curr_move_index] = Move::new_with_flags(from, to, Move::QUIET);
+                *curr_move_index += 1;
+            }
+        }
+        piece_bb &= piece_bb - 1;
+    }
 }
 
 fn generate_king_moves(board: &Board, moves: &mut [Move], curr_move_index: &mut usize) {
@@ -181,3 +230,12 @@ pub const KNIGHT_MOVES: [u64; 64] = {
     }
     table
 };
+
+// 0xFE = 11111110 (A-file is 0)
+pub const NOT_A_FILE: u64 = 0xfefefefefefefefe;
+
+// 0x7F = 01111111 (H-file is 0)
+pub const NOT_H_FILE: u64 = 0x7f7f7f7f7f7f7f7f;
+
+const BISHOP_SHIFTS: [(i8, u64); 4] = [(9, NOT_A_FILE), (7, NOT_H_FILE), (-7, NOT_A_FILE), (-9, NOT_H_FILE)];
+const ROOK_SHIFTS: [(i8, u64); 4] = [(8, !0), (-8, !0), (1, NOT_A_FILE), (-1, NOT_H_FILE)];
